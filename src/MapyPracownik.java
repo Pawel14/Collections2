a84cb46d94a91c1a5cbf10cf4d996e67534c4784
Pawel Wazny <steve91@interia.pl>
2018-10-28 10:25:27 +0100

    dodawaniezmiennoprzecinkowe

--- src/MapyPracownik.java
+++ src/MapyPracownik.java
@@ -1,2 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class MapyPracownik {
+    public static void main(String[] args) {
+
+        Map<String, String> pracownik = new HashMap<>();
+
+        pracownik.put("a111", "Jan Kowalski");
+        pracownik.put("f333", "Wojciech Kowalski");
+        pracownik.put("cd222", "Kasia Piotrowska");
+        pracownik.put("e555", "Anna Zawadzka");
+        pracownik.put("a444", "Jan Kowalski");
+
+        for (Map.Entry<String,String> wpis: pracownik.entrySet()) {
+            System.out.println(wpis.getKey()+ " "+ wpis.getValue());
+        }
+
+        Scanner ss = new Scanner(System.in);
+        System.out.print("\nwpisz klucz: ");
+        String s = ss.nextLine();
+        System.out.println("osoba o wpisanym indeksie to : "+pracownik.get(s.toLowerCase()));
+
+
+    }
+
 }
