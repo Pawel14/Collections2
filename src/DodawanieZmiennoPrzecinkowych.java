ac2c163e098e9f77df4ac4ea501a26d58acfddce
Pawel Wazny <steve91@interia.pl>
2018-10-28 09:56:12 +0100

    dodawaniezmiennoprzecinkowe

--- src/DodawanieZmiennoPrzecinkowych.java
+++ src/DodawanieZmiennoPrzecinkowych.java
@@ -1,27 +1,38 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
 public class DodawanieZmiennoPrzecinkowych {
     public static void main(String[] args) {
 
-        List<Float> lista = new ArrayList<>();
-        float a=1;
-        while(a>0.01) {
+        List<Float> floatlista = new LinkedList<>();
+//        float a=1;
+//        while(a>0.01) {
+//
+//            Random random = new Random();
+//            a = random.nextFloat()*100;
+//            lista.add((float)a);
+//        }
+        Random random = new Random();
 
-            Random random = new Random();
-            a = random.nextFloat()*100;
-            lista.add((float)a);
+        double randomValue = Math.random();
+        System.out.println("random value = "+randomValue);
+
+        while (Math.random()> 0.01 ){
+            randomValue=Math.random();
+            Float value = random.nextFloat() * 100.0f;
+            floatlista.add(value);
         }
 
-        System.out.println("Rozmiar listy to: "+lista.size());
+//            floatlista.stream().forEach(value-> System.out.println("liczba = "+value*Math.PI));
 
-        for(int i=0; i< lista.size(); i++)
+        for(int i=0; i< floatlista.size(); i++)
         {
-            System.out.println("liczba = " + lista.get(i));
-            double c = Math.PI*lista.get(i);
+            System.out.println("liczba = " + floatlista.get(i));
+            double c = Math.PI*floatlista.get(i);
 
-            System.out.println("wynik mnozenia "+lista.get(i)+"*PI =" +c);
+            System.out.println("wynik mnozenia "+floatlista.get(i)+"*PI = " +c);
         }
 
     }
